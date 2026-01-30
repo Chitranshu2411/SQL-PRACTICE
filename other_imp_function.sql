f39a423af043e5dc33321a2907a73b4e46c530a1
Chitranshu <chitranshu2411@gmail.com>
2026-01-30 23:52:54 +0530

    done
    completed

---
+++ other_imp_function.sql
@@ -0,0 +1,15 @@
+Other Important Function 
+
+1. CURDATE() --return tbe current date 
+select CURDATE();
+YYYY-MM-DD
+
+2. CURTIME() --return the current time 
+
+3. NOW() --return the current date and time 
+
+4. if (test , yes , no) --- return yes if test true else no
+
+5. IFNULL (NULL, "Test") -- if null then Test else value itself
+
+6. DATABASE()--return the current database 
